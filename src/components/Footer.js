7aacb7110106918ba028c06012f90bc6e30dcea7
Daniel Gibson <danielgibson@Daniels-MacBook-Air.local>
2019-07-04 13:28:30 +1200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
 import linkedin from '../img/social/linkedin.svg'
-import logoWhite from '../img/orchid-white.png'
+import logoWhite from '../img/orchid-White.png'
 
 const Footer = class extends React.Component {
   render() {
@@ -16,7 +16,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logoWhite}
-            alt="Kaldi"
+            alt="Orchid"
             style={{ width: '184px', height: '53.5px' }}
           />
         </div>
